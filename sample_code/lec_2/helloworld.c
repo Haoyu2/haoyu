906b9344c7e6e10ae4ea3a7658c2ceb260097156
Haoyu <haoyuumb@gmail.com>
2020-01-30 01:55:26 -0500

    lec2

---
+++ sample_code/lec_2/helloworld.c
@@ -0,0 +1,14 @@
+/* Project: HelloWorld
+ * Name   : Haoyu Wang
+ * Date   : 09/08/2019
+ * File   : hello.c
+ * Notes  : prints "hello world!"
+ */
+#include <stdio.h>
+
+int main(void)
+{
+  printf("hello, world!\n");
+  // statement which calls the function printf
+  return 0;
+}
