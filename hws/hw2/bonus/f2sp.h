d7d114dbf821a0e4e42f87156bfa6d93d8809126
Haoyu <haoyuumb@gmail.com>
2020-02-13 00:09:36 -0500

    sd

---
+++ hws/hw2/bonus/f2sp.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <assert.h>
+/* Print out a char array of 
+ * single-precision foramt of float
+ */
+void print_sp(char s[]);
+
+/* Read bits into a char array 
+*  from memory of a 32-bit data type
+*/
+void bin_char(float f, char s[]);
+
+
+
+/* Convert floats into binary char array
+*  
+*/
+void f2sp(float f, char f_sp[]);
