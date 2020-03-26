69146b3de203ecc005332f8a05e91c9fbb106480
Haoyu <haoyuumb@gmail.com>
2020-03-26 17:10:18 -0400

    hw5,6

---
+++ hws/hw5/hw5.c
@@ -0,0 +1,66 @@
+/*
+ * 
+ *
+ * project: hw5
+ * name: 
+ * user??
+ * date: 
+ * file: hw5.c
+ * notes:
+ */
+
+#include "hw5.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 30 points
+int intff(const char *s, int *nums){
+
+}
+
+
+// 30 points
+int isPalindrome(int x){
+	
+}
+
+
+
+// 40 points
+
+void tail(const char *s, int n){
+
+}
+
+
+
+// 30 points
+void strip(char *s){
+
+}
+
+
+// 30 points
+int linesff(const char *s, char **lines){
+
+}
+
+
+
+
+
+// 30 points
+int wordsfs(const char *s, char **words){
+
+
+}
+
+
+// 30 points
+char * longestCommonPrefix(char ** words, int wordsSize){
+
+
+}
+
+
