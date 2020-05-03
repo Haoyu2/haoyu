17990b8c4305ebf6152efc6bdd83ea546116e83c
Haoyu <haoyuumb@gmail.com>
2020-05-03 18:23:11 -0400

    hw8

---
+++ hws/hw8/hw8.c
@@ -0,0 +1,31 @@
+/*
+ * driver for Graham's scan
+ *
+ * project: hw7
+ * name: 
+ * user??
+ * date: 
+ * file: hw7.c
+ */
+
+
+#include "hw8.h"
+
+
+void readStudent(Student *stu, char *s){
+}
+
+
+
+Class * readClass(char *s){
+
+}
+
+
+void sortClass(Class * class){
+
+}
+
+
+void freeClass(Class * c){
+}
